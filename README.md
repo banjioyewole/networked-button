602ad244a8af1d2b1740bb57bac981ab107b8b70
Banji (Aquamarine) <efarrari@gmail.com>
2021-05-22 11:21:14 -0700

    did wire to button hardware; recieve events

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 
 ## Top Level
 
-This project makes network requests based on Joystick movement on the `SenseHAT` and display corresponding patterns on the LED Matrix. 
+This project makes network requests based on Joystick movement on the `SenseHAT` and display corresponding patterns on the LED Matrix.
 
-This is achieved by the included `network_button.py` and `gem_led_patterns.py`, the former handling joystick action parsing and network request and starting the latter with an non-blocking call to the top level `illumination_handler()` method. 
+This is achieved by the included `network_button.py` and `gem_led_patterns.py`, the former handling joystick action parsing and network request and starting the latter with an non-blocking call to the top level `illumination_handler()` method.
 
-This script is only one half. When paired with the `homebridge-http-switch` homebridge library, you can create a virtual switch that can be activated by the network requests made by `network_button`! Learn more 
+This script is only one half. When paired with the `homebridge-http-switch` homebridge library, you can create a virtual switch that can be activated by the network requests made by `network_button`! Learn more
 
 This Script makes http request to localhost to get the `homebridge-http-switch` library to activate buttons in HomeKit. The registered URLs correspond to different buttons which can be bound to different scenes within the Home app.
 
@@ -20,3 +20,9 @@ This Script makes http request to localhost to get the `homebridge-http-switch` 
 [`homebridge-http-switch`](https://www.npmjs.com/package/homebridge-http-switch]) | Add http switches to your home today!
 
 Networked-Button requires Python Library [`Requests`](https://github.com/psf/requests)
+
+
+Additional Notes for Arcade-button
+
+https://projects.drogon.net/raspberry-pi/gpio-examples/tux-crossing/3-more-leds-and-a-button/
+https://www.raspberrypi.org/forums/viewtopic.php?t=87292
--- networked_button.py
+++ networked_button.py
@@ -1,23 +1,32 @@
-from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
-from time import sleep
-from time import time
-from gem_led_patterns import *
+# from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+import RPi.GPIO as GPIO
+# from time import sleep
+# from time import time
+import time
+# from gem_led_patterns import *
 import requests
 # sense = SenseHat()
 
-e = (0, 0, 0)
-w = (255, 255, 255)
+# e = (0, 0, 0)
+# w = (255, 255, 255)
 
 # sense.clear()
 
-senseWrapper = LuminousClass()
+# senseWrapper = LuminousClass()
 # myLumen.sense.clear()
 # senseWrapper.quick_up()
+
+# The arcade-button exposes only the single press event through Homekit, handling the double press and long press with http request to other devices on the network
+
 
 # http://yourHomebridgeServerIp:webhook_port/?accessoryId=theAccessoryIdToTrigger&state=NEWSTATE
 print("Did Start Networked-Button")
 base_url = "http://localhost:"
 webhook_port = "7278"
+
+base_opal_url = "http://192.168.1.62:"
+opal_port = "42069"
+
 
 last_direction = ""
 last_type = -1
@@ -43,22 +52,34 @@ def preform_action(direction, press_type):
     formed_base_url = base_url + webhook_port
     r = requests.get(formed_base_url, params={ "accessoryId" : "sapphire_joystick" , "buttonName": direction.capitalize(), "event" : press_type })
 
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+
 while True:
-    event = senseWrapper.sense.stick.wait_for_event()
-
-    # Check if the joystick was pressed
-    if event.action == ACTION_PRESSED:
-        last_action = ACTION_PRESSED
+    if GPIO.input(3) == GPIO.LOW:
+        print("Button was pushed!")
 
 
-    elif event.action == ACTION_HELD:
-        if last_action == ACTION_HELD:
-            continue
-        last_action = ACTION_HELD
-        preform_action(event.direction, long_press)
-
-    elif event.action == ACTION_RELEASED:
-        if last_action == ACTION_HELD:
-            last_action = ACTION_RELEASED
-            continue
-        preform_action(event.direction, single_press)
+#
+# while True:
+#     event = senseWrapper.sense.stick.wait_for_event()
+#
+#     # Check if the joystick was pressed
+#     if event.action == ACTION_PRESSED:
+#         last_action = ACTION_PRESSED
+#
+#
+#     elif event.action == ACTION_HELD:
+#         if last_action == ACTION_HELD:
+#             continue
+#         last_action = ACTION_HELD
+#         preform_action(event.direction, long_press)
+#
+#     elif event.action == ACTION_RELEASED:
+#         if last_action == ACTION_HELD:
+#             last_action = ACTION_RELEASED
+#             continue
+#         preform_action(event.direction, single_press)
